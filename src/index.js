63ffd6cf1b0b5fedc0135a61a14629b7cf79e818
albertoflorence <alberto.sabatier@gmail.com>
2023-09-01 15:11:44 -0300

    requisito 4

--- src/index.js
+++ src/index.js
@@ -32,9 +32,33 @@ app.get('/talker/:id', async (req, res) => {
   return res.status(200).json(talker);
 });
 
-app.post('/login', (req, res) => {
+const validateLogin = (req, res, next) => {
   const { email, password } = req.body;
-  const token = generateToken();
 
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+    return res
+      .status(400)
+      .json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 6) {
+    return res
+      .status(400)
+      .json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+
+  next();
+};
+
+app.post('/login', validateLogin, (req, res) => {
+  const token = generateToken();
   return res.status(200).json({ token });
 });
